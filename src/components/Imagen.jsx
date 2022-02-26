aab5b33548addab509de2508c180c9b3ed773a66
Roberto Morales <roberto@morales.com.co>
2022-02-26 11:41:20 -0500

    Arreglado deploy

--- src/components/Imagen.jsx
+++ src/components/Imagen.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Imagen = () => {
   const imagen = 'https://picsum.photos/200'
     return (
-    <img src={imagen}/>
+    <img src={imagen} alt='Imagen de muestra'/>
   )
 }
 
